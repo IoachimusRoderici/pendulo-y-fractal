d78592f6dc2ec10889d0e65808b51642fe11f287
Joaquín Rodríguez <joaq.rod0@gmail.com>
2024-07-26 20:28:56 -0300

    Probar gráfico interactivo

--- source/modelo/pruebas.jl
+++ source/modelo/pruebas.jl
@@ -1,4 +1,4 @@
-using StaticArrays, DynamicalSystems
+using StaticArrays, DynamicalSystems, GLMakie
 
 include("../VectorAliases.jl")
 using .VectorAliases
@@ -17,7 +17,12 @@ function test_gravedad(; estado_inicial = Vector4D(1, 0, 0, 0),
     sistema = CoupledODEs(f, estado_inicial, params)
 
     proyección = (estado) -> Modelo.esféricas_a_cartesianas(estado[1:2], params.longitud)
-    sistema_proyectado = ProjectedDynamicalSystem(sistema, proyección, (x)->estado_inicial)
-
-    return trajectory(sistema_proyectado, tiempo, estado_inicial)
+    return ProjectedDynamicalSystem(sistema, proyección, (x)->estado_inicial)
 end
+
+sistema_proyectado = test_gravedad()
+sistema_sin_proyectar = sistema_proyectado.ds
+
+interactive_trajectory_timeseries(s.ds, [1,2,3,4], [Vector4D(1,0,0,1), Vector4D(1,0,0,0)],
+                                  idxs=(1,3), timeseries_names=["θ", "φ", "dθ/dt", "dφ/dt"],
+                                  axis=(xlabel="θ", ylabel="dθ/dt"))[1]
